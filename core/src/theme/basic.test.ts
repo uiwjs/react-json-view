5307872ff259a771c694283e937db61c7652ddec
jaywcjlove <398188662@qq.com>
2023-09-15 00:03:00 +0800

    test: add theme test case.

---
+++ core/src/theme/basic.test.ts
@@ -0,0 +1,33 @@
+import { basicTheme } from './basic';
+
+it('basicTheme test case', () => {
+  expect(basicTheme).toHaveProperty('--w-rjv-font-family', 'monospace');
+  expect(Object.keys(basicTheme)).toMatchObject([
+    '--w-rjv-font-family',
+    '--w-rjv-color',
+    '--w-rjv-key-string',
+    '--w-rjv-background-color',
+    '--w-rjv-line-color',
+    '--w-rjv-arrow-color',
+    '--w-rjv-edit-color',
+    '--w-rjv-info-color',
+    '--w-rjv-update-color',
+    '--w-rjv-copied-color',
+    '--w-rjv-copied-success-color',
+    '--w-rjv-curlybraces-color',
+    '--w-rjv-colon-color',
+    '--w-rjv-brackets-color',
+    '--w-rjv-quotes-color',
+    '--w-rjv-quotes-string-color',
+    '--w-rjv-type-string-color',
+    '--w-rjv-type-int-color',
+    '--w-rjv-type-float-color',
+    '--w-rjv-type-bigint-color',
+    '--w-rjv-type-boolean-color',
+    '--w-rjv-type-date-color',
+    '--w-rjv-type-url-color',
+    '--w-rjv-type-null-color',
+    '--w-rjv-type-nan-color',
+    '--w-rjv-type-undefined-color',
+  ]);
+});
